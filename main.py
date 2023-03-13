0d40f3379fde3f569004009a02ebf9fa8c17458f
JamieYJC <jamforce916@gmail.com>
2023-03-13 15:29:08 +0800

    test

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import csv
 
-with open("DisneylandReviews.csv","r",encoding="ISO-8859-1") as file:
+with open("./assets/DisneylandReviews.csv","r",encoding="ISO-8859-1") as file:
     reader=csv.reader(file)
     for row in reader:
         print(row)
